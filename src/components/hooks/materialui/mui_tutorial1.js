4cfb6a17c9a3c2d8a7525d2ab8fa92f69621045e
Makhmoodjan <meexample@gmail.com>
2022-04-22 19:09:25 +0300

    Material ui changes 1

---
+++ src/components/hooks/materialui/mui_tutorial1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mui_tutorial1() {
+  return (
+    <div>
+        <h2> Material ui tutorial </h2>
+    </div>
+  )
+}
+
+export default Mui_tutorial1
---
+++ src/components/hooks/react_router_2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ReactRouter2() {
+  return (
+    <div>ReactRouter2</div>
+  )
+}
+
+export default ReactRouter2
